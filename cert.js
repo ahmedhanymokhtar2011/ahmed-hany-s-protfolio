7b8935238e6bba45e6e912cc57adb13bac5ab7b1
ahmedhany54726 <136247520+ahmedhany54726@users.noreply.github.com>
2025-08-14 22:16:16 +0300

    Add files via upload

---
+++ cert.js
@@ -0,0 +1,9 @@
+/* JavaScript */
+function openModal(src) {
+  document.getElementById("modal").style.display = "block";
+  document.getElementById("modal-img").src = src;
+}
+
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
